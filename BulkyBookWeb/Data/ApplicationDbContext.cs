bc46557850288c206aac9e6636d0276e123dfbb8
Học Lê Tiến <2001191004@hufi.edu.vn>
2022-10-06 11:50:55 +0700

    test

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
